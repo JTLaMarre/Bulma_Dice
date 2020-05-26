af708d87a8a1c6302f28dc43b6457bad182fafe5
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-05-26 11:13:57 -0400

    practiced writing up clickable dice

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Bulma_Dice
+
+# Table of contents
+* [installation](#install)
+* [Usage](#Usage)
+* [Test](#Tests)
+
+# install
+open deplyed github page, maybe copy link to device to reference. 
+
+# Usage
+on click dice roll to generate numbers between 1 and X where X is D(X)
+
+# Test
+open up the deplyed page and click dice icons to generate numbers!! 
---
+++ dice.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+
+$('#D20').click(function(){
+let num = Math.floor(Math.random()*20+1)
+$('#R20').text('results:'+num)
+})
+$('#D12').click(function(){
+let num = Math.floor(Math.random()*12+1)
+$('#R12').text('results:'+num)
+})
+$('#D10').click(function(){
+let num = Math.floor(Math.random()*10+1)
+$('#R10').text('results:'+num)
+})
+$('#D8').click(function(){
+let num = Math.floor(Math.random()*8+1)
+$('#R8').text('results:'+num)
+})
+$('#D6').click(function(){
+let num = Math.floor(Math.random()*6+1)
+$('#R6').text('results:'+num)
+})
+$('#D4').click(function(){
+let num = Math.floor(Math.random()*4+1)
+$('#R4').text('results:'+num)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
